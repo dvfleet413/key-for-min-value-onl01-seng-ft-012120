860ad4d4930ca189f14c6f2fc51738625478ffa8
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 21:57:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     values.each do |value|
       min_value = value if value < min_value 
     end
-    name_hash.collect do |key, value|
+    name_hash.each do |key, value|
      return key if value == min_value
     end 
 end
