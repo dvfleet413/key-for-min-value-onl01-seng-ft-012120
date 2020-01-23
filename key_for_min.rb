5ee2aa39cb4cfd4421c842ae4d798867bffe5bf4
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 21:46:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  result = nil
+  name_hash.each do |key, value|
+    if vaule > result
+  end 
 end
